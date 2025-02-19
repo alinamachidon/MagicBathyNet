2ca55b6a0c5736ffe3a1032e417ef4a1f03aa605
Alina Machidon <alina.machidon@fri.uni-lj.si>
2025-02-19 14:59:29 +0000

    Updated

--- test_bathymetry_orthotiles.py
+++ test_bathymetry_orthotiles.py
@@ -40,7 +40,6 @@ import gdal
 from bathymetry.magicbathy_unet import *
 # Dataset
 from dataset_bathymetry import dataset
-from custom_loss import CustomLoss
 from utils import *
 
 # Utility Libraries
@@ -48,7 +47,7 @@ from glob import glob
 from IPython.display import clear_output
 from torchvision.transforms import RandomCrop, Resize
 from tqdm import tqdm_notebook as tqdm
-from skimage import exposure
+from PIL import Image
 
 
 location = "agia_napa"
@@ -94,7 +93,7 @@ elif dataset == "S2":
     BATCH_SIZE = 1
     train_images = ['409', '418', '350', '399', '361', '430', '380', '359', '371', '377', '379', '360', '368', '419', '389', '420', '401', '408', '352', '388', '362', '421', '412', '351', '349', '390', '400', '378']
     test_images = ['411', '387', '410', '398', '370', '369', '397']
-    
+
 net = UNet_bathy(3, 1)
 base_lr = 0.0001
 
@@ -117,8 +116,10 @@ crop_size = 256
 pad_size = 32  # Define pad size here
 
 
-def read_water_tiles_only():
-    water_tiles_file = ("water_tiles_new.txt")
+def read_water_tiles_only(tile_size):
+    # water_tiles_file = ("water_tiles_list_1440.txt")
+    water_tiles_file = (f"water_tiles_list_{tile_size}.txt")
+    
     # Print the absolute path to check if it's correct
     print("File path:", os.path.abspath(water_tiles_file))
 
@@ -132,17 +133,17 @@ def read_water_tiles_only():
             raise ValueError("No water tiles found in the file.")
         # Load images using the filenames from water_tiles.txt
 
-        base_path = os.path.expanduser("~/drone_processing/data/chips/")
+        #base_path = os.path.expanduser(f"~/drone_processing/data/chips_{tile_size}/")
+        base_path = os.path.expanduser(f"~/drone_processing/data/chips_{tile_size}/")
         
-        # for tile_name in water_tile_names:
-        #     print(os.path.abspath(os.path.join("../../../../drone_processing/data/chips/",tile_name)))
-        #     print(os.path.abspath(os.path.join(base_path, tile_name)))
 
         test_images = (
             (np.asarray(io.imread(os.path.abspath(os.path.join(base_path, tile_name))), dtype="float32") - norm_param[0]) /
             (norm_param[1] - norm_param[0])
+            
             for tile_name in water_tile_names
         )
+        
         print(f" Successfully loaded {len(water_tile_names)} water tile images.")
         return test_images, water_tile_names
     
@@ -154,10 +155,30 @@ def read_water_tiles_only():
         print(f" Unexpected error: {e}")
 
 
-def test_on_orthophoto(net):
+def read_all_tiles(tile_size):
+    print(f"~/drone_processing/data/chips_{tile_size}/")
+    base_path = os.path.expanduser(f"~/drone_processing/data/chips_{tile_size}/")
+    # List all files in the directory
+    file_names = os.listdir(base_path)
+
+    test_images = (
+        (np.asarray(io.imread(os.path.abspath(os.path.join(base_path, tile_name))), dtype="float32") - norm_param[0]) /
+        (norm_param[1] - norm_param[0])
+        
+        for tile_name in file_names
+    )
+
+    return test_images, file_names
+
+
+
+def test_on_orthophoto(net, tile_size):
     # Use the network on the test set
     #test_images = ((np.asarray(io.imread(DATA_FOLDER.format(id)), dtype='float32') - norm_param[0]) / (norm_param[1] - norm_param[0]) for id in test_ids)
-    test_images, water_tiles_names =  read_water_tiles_only()
+    
+    #test_images, water_tiles_names =  read_water_tiles_only()
+    test_images, water_tiles_names = read_all_tiles(tile_size)
+    
    
     # Switch the network to inference mode
     net.eval()
@@ -195,29 +216,34 @@ def test_on_orthophoto(net):
         # plt.close()
 
         # Plot the first image (side 1)
+         
         ax1 = fig.add_subplot(1, 2, 1)  # Layout: 1 row, 2 columns, 1st subplot
         cax1 = ax1.imshow(np.asarray(255 * img, dtype='uint8'))  # Convert to uint8 if needed
         ax1.set_title("Original Image")
         ax2 = fig.add_subplot(1, 2, 2)  
-        cax2 = ax2.imshow(pred)
+        cax2 = ax2.imshow(pred*norm_param_depth)
         ax2.set_title("Predicted Depth")
         # Add a colorbar for the second subplot (depth prediction)
         #fig.colorbar(cax2, ax=ax2, orientation='vertical', label='Depth Values')
         # Add a single colorbar (linked to the first image's plot)
-        fig.colorbar(cax2, ax=[ax1, ax2], orientation='vertical', label='Depth Values', fraction=0.02, pad=0.04)
-
-        fig.savefig(f"soca/uav_{location}/test_image_result_{filename[:-4]}.png")
+        fig.colorbar(cax2, ax=[ax1, ax2], orientation='vertical', label='Depth Values', fraction=0.02, pad=0.05)
+        plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+        fig.savefig(f"soca/uav_{location}/tiles_{tile_size}/test_image_result_{filename[:-4]}.png")
         plt.close()
-       
         
 
-if dataset=="S2":
-    net.load_state_dict(torch.load('./bathymetry_s2_an'))
-elif dataset=="UAV":
-    #net.load_state_dict(torch.load('./bathymetry_aerial_pl'))
-    net.load_state_dict(torch.load('./bathymetry_aerial_an'))
-else:
-    net.load_state_dict(torch.load('./bathymetry_spot6_an'))
+def main():
+    if dataset=="S2":
+        net.load_state_dict(torch.load('./bathymetry_s2_an'))
+    elif dataset=="UAV":
+        #net.load_state_dict(torch.load('./bathymetry_aerial_pl'))
+        net.load_state_dict(torch.load('./bathymetry_aerial_an'))
+    else:
+        net.load_state_dict(torch.load('./bathymetry_spot6_an'))
 
-test_on_orthophoto(net)
+    test_on_orthophoto(net, tile_size=2880)
+
+if __name__ == "__main__":
+    main()
+
 
